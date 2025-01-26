f94098bb822737dad9d71547015c64db72a83772
Kunal Pal <kunalpal460@gmail.com>
2025-01-26 21:00:22 +0530

    Adding DSA A to Z Folder/files

---
+++ Arrays/find_largest_element.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int max_element(int arr[] , int n){
+
+    if(n == 0){
+        cout<<"Does Not have Any element";
+        return 0;
+    }
+
+    int maxi = INT_MIN;
+
+    for(int i=0; i<n; i++){
+        if(arr[i] > maxi){
+            maxi = arr[i];
+        }
+    }
+    return maxi;
+}
+
+
+int min_element(int arr[] , int n){
+
+    if(n == 0){
+        cout <<"Array does not contain any element" <<endl;
+        return 0;
+    }
+
+
+    int mini = INT_MAX;
+
+    for(int i=0; i<n; i++){
+        if(arr[i] < mini){
+            mini = arr[i];
+        }
+    }
+
+    return mini;
+}
+
+int main(){
+
+
+    int n; 
+    cout << "Enter the Size of Array : ";
+    cin >> n;
+
+    int arr[n];
+
+    cout<< endl <<"Enter the Array Elements : ";
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int max_ans = max_element(arr , n);
+
+    cout << endl << "Largest Element in Array is : " << max_ans <<endl;
+
+
+    int min_ans = min_element(arr , n);
+
+    cout << "Minimum Element in array is " << min_ans <<endl;
+
+    return 0;
+}
